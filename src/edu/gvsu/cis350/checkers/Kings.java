fd93e74ca0ceac0e9c26403febaaea38a49ff90e
JakeYoung187 <youngjac@mail.gvsu.edu>
2016-10-17 22:34:22 -0400

    isvalidmove is updated

--- src/edu/gvsu/cis350/checkers/Kings.java
+++ src/edu/gvsu/cis350/checkers/Kings.java
@@ -36,19 +36,59 @@ public class Kings extends GamePiece {
 	public final boolean isValidMove(
 			             final Move move, final IGamePiece[][] board) {
 		
-		if (Math.abs(move.fromRow - move.toRow) > 1) {
-			return false;
+		if (move.fromRow != move.toRow && move.fromColumn != move.toColumn) {
+			if (Math.abs(move.fromRow - move.toRow) == 1) {
+				if (Math.abs(move.fromColumn - move.toColumn) == 1) {
+					return true;
+				}
+			}
+			//Jumps
+			//up
+			if (move.toRow - move.fromRow == 2) {
+				//right
+				if (move.toColumn - move.fromColumn == 2) {
+					if (board[move.fromRow + 1][move.fromColumn + 1] != null) {
+						if (board[move.fromRow + 1][move.fromColumn + 1]
+								.player() != owner) {
+							return true;
+						}
+					}
+				}
+				//left
+				if (move.fromColumn - move.toColumn == 2) {
+					if (move.fromColumn - move.toColumn == 2) {
+						if (board[move.fromRow + 1][move.fromColumn - 1] 
+								!= null) {
+							if (board[move.fromRow + 1][move.fromColumn - 1]
+									.player() != owner) {
+								return true;
+							}
+						}
+					}
+				}
+			}
+			//down jump
+			if (move.fromRow - move.toRow == 2) {
+				//right
+				if (move.toColumn - move.fromColumn == 2) {
+					if (board[move.fromRow - 1][move.fromColumn + 1] != null) {
+						if (board[move.fromRow - 1][move.fromColumn + 1]
+								.player() != owner) {
+							return true;
+						}
+					}
+				}
+				//left
+				if (move.fromColumn - move.toColumn == 2) {
+					if (board[move.fromRow - 1][move.fromColumn - 1] != null) {
+						if (board[move.fromRow - 1][move.fromColumn - 1]
+								.player() != owner) {
+							return true;
+						}
+					}
+				}
+			}
 		}
-		
-		if (Math.abs(move.fromColumn - move.toColumn) > 1) {
-			return false;
-		}
-		
-		if (move.fromRow == move.toRow || move.fromColumn == move.toColumn) {
-			return false;
-			
-		} else {
-			return true;
-		}
+		return false;
 	}
 }
